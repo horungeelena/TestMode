0d62dc5db24746d87925e4cad9242426306b589f
Elena Fedoseeva <horungeelena@gmail.com>
2021-03-05 20:59:39 +0200

    Re-modified *.java.

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -5,6 +5,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import lombok.val;
 
 import java.util.Locale;
 
@@ -38,14 +39,17 @@ public class DataGenerator {
     }
 
     public static Registration generateInvalidLoginForCustomer() {
-        Registration invalidLogin = new Registration("логин",faker.internet().password(),"active");
+        val password = faker.internet().password();
+        val invalidLogin = new Registration(faker.name().firstName(), password, "active");
         requestForm(invalidLogin);
-        return new Registration(faker.name().firstName(),"пароль","active");
+        return new Registration(faker.name().firstName(), password, "active");
     }
 
     public static Registration generateInvalidPasswordForCustomer() {
-        Registration invalidPassword = new Registration(faker.name().firstName(),"пароль","active");
+        val login = faker.name().firstName();
+        val invalidPassword = new Registration(login, faker.internet().password(), "active");
         requestForm(invalidPassword);
-        return new Registration("логин",faker.internet().password(),"active");
+        return new Registration(login, faker.internet().password(), "active");
     }
+
 }
